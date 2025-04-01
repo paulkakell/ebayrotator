982e376f98b6f9b18a080a458d9d569ed4cf7e3d
Paul K <59290939+paulkakell@users.noreply.github.com>
2025-04-01 13:29:53 -0600

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,5 @@
+class Setting(Base):
+    __tablename__ = "settings"
+    id = Column(Integer, primary_key=True, index=True)
+    key = Column(String(255), unique=True, index=True, nullable=False)
+    value = Column(Text, nullable=False)
